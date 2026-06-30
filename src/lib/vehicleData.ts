b3a510100390c675f7e653e425b5256765034395
Chanuka99 <m.chanuka99@gmail.com>
2026-06-30 16:56:06 +0530

    remove unused build log files

--- src/lib/vehicleData.ts
+++ src/lib/vehicleData.ts
@@ -3,7 +3,7 @@
 export const BRANDS = [
   { name: "Toyota", models: ["Corolla","Axio","Premio","Allion","Vitz","Aqua","Prius","Raize","CHR","Land Cruiser Prado","Land Cruiser","Rush","KDH","Hiace","Noah","Voxy","Other"] },
   { name: "Suzuki", models: ["Alto","Wagon R","Baleno","Celerio","Swift","Every","Spacia","Other"] },
-  { name: "Nissan", models: ["Sunny","X-Trail","Leaf","March","Dayz","Caravan","Other"] },
+  { name: "Nissan", models: ["Sunny","X-Trail","Leaf","March","Dayz","Caravan","magnite ","Other"] },
   { name: "Honda", models: ["Fit","Vezel","Civic","Grace","CR-V","insight","Other"] },
   { name: "Mitsubishi", models: ["Lancer","Outlander","Montero","Pajero","Eclipse Cross","Other"] },
   { name: "Mazda", models: ["Demio","Axela","CX-5","Other"] },
